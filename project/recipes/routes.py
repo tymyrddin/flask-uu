47eb546d83af4e97f95670c38418e3aa518f4728
ninabarzh <30133030+ninabarzh@users.noreply.github.com>
2022-05-17 13:14:03 +0200

    Added footer

--- project/recipes/routes.py
+++ project/recipes/routes.py
@@ -1,9 +1,7 @@
 from . import recipes_blueprint
-from flask import render_template, abort
+from flask import render_template
 
 
 @recipes_blueprint.route("/")
 def recipes():
     return render_template("recipes/recipes.html")
-
-
