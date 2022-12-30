4f5f9fb13bf741e3ff8cac7ad4a8eb2966e4a709
ninabarzh <30133030+ninabarzh@users.noreply.github.com>
2022-12-30 22:21:59 +0100

    Second layer pages for labs and scripting

--- project/recipes/routes.py
+++ project/recipes/routes.py
@@ -1,7 +1,34 @@
 from . import recipes_blueprint
 from flask import render_template
 
+labs_recipes_names = [
+    "labs-thm",
+    "labs-portswigger",
+    "labs-htb",
+    "labs-rootme",
+    "labs-btlo",
+    "labs-offsec",
+]
+
+scripts_recipes_names = [
+    "scripts-network",
+    "scripts-webapp",
+    "scripts-cicd",
+    "scripts-bash",
+    "scripts-ps",
+]
+
 
 @recipes_blueprint.route("/")
 def recipes():
     return render_template("recipes/recipes.html")
+
+
+@recipes_blueprint.route("/labs/")
+def labs_recipes():
+    return render_template("recipes/labs.html")
+
+
+@recipes_blueprint.route("/scripts/")
+def scripts_recipes():
+    return render_template("recipes/scripts.html")
