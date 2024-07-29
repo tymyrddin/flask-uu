bf5002a70288d26aa873adad0053393d816b7a04
ninabarzh <30133030+ninabarzh@users.noreply.github.com>
2024-07-30 01:17:20 +0200

    Removed fpnt awesome dependency

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
 from flask_frozen import Freezer
 from project import create_app
-from flask_font_awesome import FontAwesome
 
 
 # Call the application factory function to construct a Flask application
 # instance using the development configuration
 app = create_app()
-font_awesome = FontAwesome(app)
 
 # Create an instance of Freezer for generating the static files from
 # the Flask application routes ('/', '/breakfast', etc.)
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,10 @@
 import pytest
 from project import create_app
-from flask_font_awesome import FontAwesome
 
 
 @pytest.fixture(scope="module")
 def test_client():
     flask_app = create_app()
-    font_awesome = FontAwesome(flask_app)
 
     # Create a test client using the Flask application configured for testing
     with flask_app.test_client() as testing_client:
